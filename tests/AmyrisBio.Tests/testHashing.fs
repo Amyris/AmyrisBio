d1ee04170985419363eee79fcc0ced3861e6fd08
Amyris <github@feb17.org>
2016-09-23 17:04:41 -0700

    ENH: bug fixing the comment for MerHash, adding some helper functions and testing

---
+++ tests/AmyrisBio.Tests/testHashing.fs
@@ -0,0 +1,50 @@
+﻿module testHashing
+
+open Amyris.Bio.MerHash
+open NUnit.Framework
+
+let rc (s:string) = Amyris.Bio.biolib.revComp (s.ToCharArray()) |> Amyris.Bio.utils.arr2seq
+let mer1 = "ATGTCATGCTACCCGTGTAATCTGATCATGTA"
+let mer1RC = rc mer1
+
+let template2 = "ATGTCATGCTACCCGTGTAATCTGATCATGTAC"
+let query2 =     "TGTCATGCTACCCGTGTAATCTGATCATGTAC"
+let query2RC = rc query2
+
+
+let template3 = "ATGTCATGCTACCCGTGTAATCTGATCATGTACTTGACTAGCTAGCTGACGTATGCTACTGACTGAGCACGTATCTACGTACGGCGCGATCTGATCATCTGACACACACACTAGCTACTATCCCAGTTTTTTG"
+let query3 =     "GTATCTACGTACGGCGCGATCTGATCATCTGA"
+let query3RC = rc query3
+
+
+        
+[<Test>]
+let T1_hashOne() =
+    let h = hashTemplate32 32 mer1
+    Assert.AreEqual((ONCE,0) ,search32 h mer1)
+
+[<Test>]
+let T1_hashOneRC() =
+    let h = hashTemplate32 32 mer1
+    Assert.AreEqual((ONCE,0),search32 h mer1RC)
+
+
+[<Test>]
+let T2_hashOne() =
+    let h = hashTemplate32 32 template2
+    Assert.AreEqual((ONCE,0) ,search32 h query2)
+
+[<Test>]
+let T2_hashOneRC() =
+    let h = hashTemplate32 200 template2
+    Assert.AreEqual((ONCE,0),search32 h query2)
+
+[<Test>]
+let T3_hashOne() =
+    let h = hashTemplate32 200 template3
+    Assert.AreEqual((ONCE,0) ,search32 h query3)
+
+[<Test>]
+let T3_hashOneRC() =
+    let h = hashTemplate32 200 template3
+    Assert.AreEqual((ONCE,0),search32 h query3)
---
+++ tests/AmyrisBio.Tests/testSmithWaterman.fs
@@ -0,0 +1,77 @@
+﻿module testSmithWaterman
+open System
+open NUnit.Framework
+
+let run (a:string) (b:string) =
+    let a' = a.Replace("-","")
+    let b' = b.Replace("-","")
+    let ok,alignA,alignB,_ = Amyris.Bio.smithWaterman._smithWaterman false None 10 20 (a'.ToCharArray()) (b'.ToCharArray())
+    if alignA <> a || alignB <> b then
+        Assert.Fail(sprintf "mismatch checking alignment:\n%s\n%s\n%s\n%s" a b alignA alignB)
+    ()
+
+//    let a = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"    
+//    let b = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+[<Test>]
+let T1_simpleSW() =
+    let a = "TGATCTACTGACGTAT-TGATGCATCACTGATCTA"    
+    let b = "TGATCTACT-ACGTATCTGATGCATCACTGATCTA"
+    run a b
+
+[<Test>]
+let T2_leadingGaps1() =
+    let a = "----------ACGTATCTGATGCATCACTGATCTA"    
+    let b = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+    run a b
+let T3_leadingGaps2() =
+    let b = "----------ACGTATCTGATGCATCACTGATCTA"    
+    let a = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+    run a b
+
+
+[<Test>]
+let T4_trailingGaps1() =
+    let a = "TGATCTACTGACGTATCTGATGC------------"    
+    let b = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+    run a b
+
+[<Test>]
+let T5_trailingGaps2() =
+    let b = "TGATCTACTGACGTATCTGATGC------------"    
+    let a = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+    run a b
+
+[<Test>]
+let T6_interiorGaps1() =
+    let a = "TGATCTACTG------------------TGATCTA"    
+    let b = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+    run a b
+
+[<Test>]
+let T7_interiorGaps2() =
+    let b = "TGATCTACTG------------------TGATCTA"    
+    let a = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+    run a b
+
+
+(*
+testSmithWaterman.test:
+mismatch checking alignment:
+TGATCTACTG------------------TGATCTA  7 * 4 - 18 = 10
+TGATCTACTGACGTATCTGATGCATCACTGATCTA
+
+
+TGATCTACTGT-G-ATCT-A---------------  6 * 4 - 18 - 1= 5
+TGATCTACTGACGTATCTGATGCATCACTGATCTA
+*)
+
+[<Test>]
+let test() =
+    let a = "TGATCTACTG------------------TGATCTA"    
+    let b = "TGATCTACTGACGTATCTGATGCATCACTGATCTA"
+    let a' = a.Replace("-","")
+    let b' = b.Replace("-","")
+    let ok,alignA,alignB,_ = Amyris.Bio.smithWaterman._smithWaterman true None 10 40 (a'.ToCharArray()) (b'.ToCharArray())
+    if alignA <> a || alignB <> b then
+        Assert.Fail(sprintf "mismatch checking alignment:\n%s\n%s\n%s\n%s" a b alignA alignB)
+    ()
