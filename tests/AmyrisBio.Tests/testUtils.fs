7ac910ef84983a26542ccf2a42bc888e24a33853
general electrix <general.electrix@gmail.com>
2016-10-18 21:51:17 -0700

    TST: add tests for expected behavior of format60

--- tests/AmyrisBio.Tests/testUtils.fs
+++ tests/AmyrisBio.Tests/testUtils.fs
@@ -19,3 +19,19 @@ let testPaddedZip() =
     check [] [] []
     check [1] [] [Some(1), None]
     check [] [1] [None, Some(1)]
+
+[<Test>]
+let testFormat60() =
+    Assert.AreEqual("", format60 [||])
+    Assert.AreEqual("A", format60 [|'A'|])
+    Assert.AreEqual("AT", format60 [|'A'; 'T'|])
+
+
+    let A60 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    let A60Arr = [|for c in A60 -> c|]
+
+    let A61Arr = Array.append A60Arr [|'A'|]
+    let A61Result = A60 + "\nA"
+
+    Assert.AreEqual(A60, format60 A60Arr)
+    Assert.AreEqual(A61Result, format60 A61Arr)
