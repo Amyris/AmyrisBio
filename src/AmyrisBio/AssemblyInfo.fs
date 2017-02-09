5109fde331639dcfa67b49606d0873310ee3e42d
general electrix <general.electrix@gmail.com>
2017-02-09 14:30:57 -0800

    VER: 2.0.18

--- src/AmyrisBio/AssemblyInfo.fs
+++ src/AmyrisBio/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("AmyrisBio")>]
 [<assembly: AssemblyProductAttribute("AmyrisBio")>]
 [<assembly: AssemblyDescriptionAttribute("Amyris FSharp BioLib")>]
-[<assembly: AssemblyVersionAttribute("2.0.17")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.17")>]
+[<assembly: AssemblyVersionAttribute("2.0.18")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.18")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.0.17"
-    let [<Literal>] InformationalVersion = "2.0.17"
+    let [<Literal>] Version = "2.0.18"
+    let [<Literal>] InformationalVersion = "2.0.18"
