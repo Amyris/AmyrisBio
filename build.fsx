47944f11c6c5b094655881af2d5b3cd6a2c4d39a
Marcell Legeza <marcell@demetrixbio.com>
2019-05-14 13:41:50 +0200

    MNT: syncrhonizing tool versions with build dependency versions

--- build.fsx
+++ build.fsx
@@ -163,6 +163,8 @@ Target.create "Build" (fun _ ->
 Target.create "RunTests" (fun _ ->
     DotNet.test id "tests/AmyrisBio.Tests")
 
+let paketPath = Path.Combine(".", ".tool", (if Environment.isWindows then "paket.exe" else "paket"))
+
 // --------------------------------------------------------------------------------------
 // Build a NuGet package
 
@@ -170,7 +172,7 @@ Target.create "NuGet" (fun _ ->
     Paket.pack(fun p ->
         { p with
             // Workaround until this is fixed: https://github.com/fsharp/FAKE/issues/2242
-            ToolPath = Path.Combine(".", ".paket", (if Environment.isWindows then "paket.exe" else "paket"))
+            ToolPath = paketPath
             OutputPath = "bin"
             Version = release.NugetVersion
             ReleaseNotes = String.toLines release.Notes})
@@ -180,7 +182,7 @@ Target.create "PublishNuget" (fun _ ->
     Paket.push(fun p ->
         { p with
             // Workaround until this is fixed: https://github.com/fsharp/FAKE/issues/2242
-            ToolPath = Path.Combine(".", ".paket", (if Environment.isWindows then "paket.exe" else "paket"))
+            ToolPath = paketPath
             WorkingDir = "bin" })
 )
 
